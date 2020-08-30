03d2e37a740075e104755a5bafd2cf74ab0c719c
Thiago Carrilho Rodriguez <tcr@discente.ifpe.edu.br>
2020-08-29 21:31:14 -0300

    Remove SSL config from Prod DB Config

--- src/config/database.js
+++ src/config/database.js
@@ -32,11 +32,5 @@ module.exports = {
       'timestamps': true,
       'underscored': true,
     },
-    'dialectOptions': {
-      'ssl': {
-        'require': true,
-        'rejectUnauthorized': false,
-      },
-    },
   },
 };
