7030a6b32789d14ca532055d054cd9fdfae0b141
Thiago Carrilho <ticocarrilho@gmail.com>
2020-11-26 23:31:18 -0300

    client: Add Post directly to Redux State, instead of fetching all posts again, when making a post

--- client/src/components/Post/PostInput.js
+++ client/src/components/Post/PostInput.js
@@ -10,7 +10,7 @@ import SendIcon from '@material-ui/icons/Send';
 import { useDispatch } from 'react-redux';
 import { useForm } from 'react-hook-form';
 import { makeStyles } from '@material-ui/core/styles';
-import { fetchPosts } from '../../slices/postSlice';
+import { addPost } from '../../slices/postSlice';
 import api from '../../services/api';
 
 const useStyles = makeStyles((theme) => ({
@@ -25,25 +25,22 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const PostInput = ({ post }) => {
+const PostInput = () => {
   const classes = useStyles();
   const { register, handleSubmit, errors, setError, clearErrors } = useForm();
   const dispatch = useDispatch();
 
   const onSubmit = async (data) => {
-    try {
-      const response = await api.post('post', {
+    try { //!!TODO Add Confirmation Dialog
+      const post = {
         title: data.title,
-        content: data.content
-      }, { withCredentials: true });
-      if(response.status===201){
-        dispatch(fetchPosts())
-        /*!!TODO
-          Add Post to Redux instead of making a fetchPosts() request
-          Add a confirmation alert
-        */
+        content: data.content,
+      };
+      const response = await api.post('post', post, { withCredentials: true });
+      if (response.status === 201) {
+        dispatch(addPost(response.data));
       }
-    } catch ({ response }) {
+    } catch (response) {
       const { data } = response;
       if (Array.isArray(data?.error)) {
         data.error.forEach((error) => {
@@ -54,13 +51,15 @@ const PostInput = ({ post }) => {
         });
       }
     }
-  }
+  };
 
   return (
     <Card>
       <CardHeader title='Create New Post' />
       <CardContent>
-        <form className={classes.formContainter} onSubmit={handleSubmit(onSubmit)}>
+        <form
+          className={classes.formContainter}
+          onSubmit={handleSubmit(onSubmit)}>
           <TextField
             inputRef={register({
               required: { value: true, message: 'The title is required.' },
