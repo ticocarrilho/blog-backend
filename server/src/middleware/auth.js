b424167f1b56299b8dc0354d89e4b03b5a6aa39a
Thiago Carrilho <ticocarrilho@gmail.com>
2020-11-26 23:25:36 -0300

    server: Verify JWT Promise, Fix Auth Middleware Error

--- server/src/middleware/auth.js
+++ server/src/middleware/auth.js
@@ -1,6 +1,17 @@
 const jwt = require('jsonwebtoken');
 const { User } = require('../app/models');
 
+const verifyJwt = (token) => {
+  return new Promise((resolve, reject) => {
+    jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
+      if (err) {
+        reject(err);
+      }
+      resolve(decoded.id);
+    });
+  }) 
+}
+
 module.exports = {
   async auth(req, res, next) {
     if (req.cookies.session == 1) {
@@ -10,22 +21,24 @@ module.exports = {
           error: 'Token not provided!',
         });
       }
+
       let id;
-      jwt.verify(token, process.env.JWT_SECRET, (err, decoded) => {
-        if (err) {
-          return res.status(401).json({
-            error: 'Unauthorized!',
-          });
-        }
-        id = decoded.id;
-      });
+
+      verifyJwt(token).then((decodedId) => {
+        id = decodedId;
+      }), err => {
+        return res.status(401).json({
+          error: 'Unauthorized!',
+        })
+      }
+
       try {
         const user = await User.findByPk(id);
         if (!user) {
           return res.status(404).json({ error: 'User does not exists.' });
         }
         req.user = id;
-        return next();
+        next();
       } catch (error) {
         return res.status(500).json({ message: 'Server error.' });
       }
